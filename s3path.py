23de1c30b7e74ef62786d48e21e7afc8fe5d5e82
lior <li.mizr@gmail.com>
2018-10-01 15:52:17 +0300

    implement glob,rglob, and iterdir

--- s3path.py
+++ s3path.py
@@ -1,12 +1,46 @@
 """
 This library wrap's the boto3 api to provide a more Pythonice API to S3
 """
-from pathlib import PurePath, _PosixFlavour
+from contextlib import suppress
+from collections import namedtuple
+from posix import DirEntry
+from pathlib import _PosixFlavour, _Accessor, _make_selector, _RecursiveWildcardSelector, PurePath, Path
 
 try:
     import boto3
+    from botocore.exceptions import ClientError
 except ImportError:
     boto3 = None
+    ClientError = Exception
+
+
+StatResult = namedtuple('StatResult', 'size, last_modified')
+
+
+class S3DirEntry:
+    def __init__(self, name, is_dir, size=None, last_modified=None):
+        self.name = name
+        self._is_dir = is_dir
+        self._stat = StatResult(size=size, last_modified=last_modified)
+
+    def __repr__(self):
+        return '{}(name={}, is_dir={}, stat={})'.format(
+            type(self).__name__, self.name, self._is_dir, self._stat)
+
+    def inode(self, *args, **kwargs):
+        return None
+
+    def is_dir(self):
+        return self._is_dir
+
+    def is_file(self):
+        return not self._is_dir
+
+    def is_symlink(self, *args, **kwargs):
+        return False
+
+    def stat(self):
+        return self._stat
 
 
 class _S3Flavour(_PosixFlavour):
@@ -25,23 +59,219 @@ class _S3Flavour(_PosixFlavour):
         uri = super().make_uri(path)
         return uri.replace('file:///', 's3://')
 
+    def casefold(self, s):
+        return s
+
+    def casefold_parts(self, parts):
+        return parts
+
+
+class _S3Accessor(_Accessor):
+    """
+    An accessor implements a particular (system-specific or not)
+    way of accessing paths on the filesystem.
+
+    In this case this will access AWS S3 service
+    """
+    def __init__(self):
+        self.s3 = boto3.resource('s3')
+
+    def stat(self, path):
+        simple_object = self.s3.ObjectSummary(path.bucket, path.key)
+        return StatResult(
+            size=simple_object.size,
+            last_modified=simple_object.last_modified,
+        )
+
+    def is_dir(self, path):
+        bucket = self.s3.Bucket(path.bucket)
+        return any(bucket.objects.filter(Prefix=self._generate_prefix(path)))
+
+    def exists(self, path):
+        if not path.bucket:
+            return any(self.s3.buckets.all())
+        if not path.key:
+            bucket = self.s3.Bucket(path.bucket)
+            return any(bucket.objects.all())
+        bucket = self.s3.Bucket(path.bucket)
+        for object in bucket.objects.filter(Prefix=path.key):
+            if object.key == path.key:
+                return True
+            if object.key.startswith(path.key + path._flavour.sep):
+                return True
+        return False
+
+    def scandir(self, path):
+        if not path.bucket:
+            for bucket in self.s3.buckets.all():
+                yield S3DirEntry(bucket.name, is_dir=True)
+            return
+        bucket = self.s3.Bucket(path.bucket)
+        sep = path._flavour.sep
+
+        response = bucket.meta.client.list_objects(
+            Bucket=bucket.name,
+            Prefix=self._generate_prefix(path),
+            Delimiter=sep)
+        for folder in response.get('CommonPrefixes', ()):
+            full_name = folder['Prefix'][:-1] if folder['Prefix'].endswith(sep) else folder['Prefix']
+            name = full_name.split(sep)[-1]
+            yield S3DirEntry(name, is_dir=True)
+        for file in response.get('Contents', ()):
+            name = file['Key'].split(sep)[-1]
+            yield S3DirEntry(name=name, is_dir=False, size=file['Size'], last_modified=file['LastModified'])
+
+    def listdir(self, path):
+        for entry in self.scandir(path):
+            yield entry.name
+
+    def _generate_prefix(self, path):
+        sep = path._flavour.sep
+        if not path.key:
+            return ''
+        if not path.key.endswith(sep):
+            return path.key + sep
+        return path.key
+
+
 _s3_flavour = _S3Flavour()
+_s3_accessor = _S3Accessor()
 
 
 class PureS3Path(PurePath):
+    """
+    PurePath subclass for AWS S3 service.
+
+    S3 is not a file-system but we can look at it like a POSIX system.
+
+    # todo: finish the doc's
+    # instantiating a PurePath should return this object.
+    # However, you can also instantiate it directly on any system.
+    """
     _flavour = _s3_flavour
     __slots__ = ()
 
-    # @classmethod
-    # def _from_parts(cls, args, init=True):
-    #     # We need to call _parse_args on the instance, so as to get the
-    #     # right flavour.
-    #     self = object.__new__(cls)
-    #     import ipdb; ipdb.set_trace()
-    #     drv, root, parts = self._parse_args(args)
-    #     self._drv = drv
-    #     self._root = root
-    #     self._parts = parts
-    #     if init:
-    #         self._init()
-    #     return self
+    @classmethod
+    def from_uri(cls, uri):
+        if not uri.startswith('s3:/'):
+            raise ValueError('...')
+        return cls(uri[4:])
+
+    @property
+    def bucket(self):
+        """
+        Returns a Path
+        :return:
+        """
+        if not self.root:
+            return ''
+        with suppress(ValueError):
+            _, bucket, *_ = self.parts
+            return bucket
+        return ''
+
+    @property
+    def key(self):
+        if not self.root:
+            return ''
+        if not self.bucket:
+            return ''
+        key_starts_index = self.parts.index(self.bucket) + 1
+        return self._flavour.sep.join(self.parts[key_starts_index:])
+
+
+class S3Path(Path, PureS3Path):
+    """Path subclass for AWS S3 service.
+
+    S3 is not a file-system but we can look at it like a POSIX system.
+
+    # todo: finish the doc's
+    # On a POSIX system, instantiating a Path should return this object.
+    """
+    __slots__ = ()
+    _NOT_SUPPORTED_MESSAGE = '{method} is unsupported on S3 service'
+
+    def stat(self):
+        self._absolute_path_validation()
+        if not self.bucket or not self.key:
+            return None
+        return super().stat()
+
+    def exists(self):
+        self._absolute_path_validation()
+        return self._accessor.exists(self)
+
+    def is_dir(self):
+        self._absolute_path_validation()
+        if not self.exists():
+            return False
+        if self.bucket and not self.key:
+            return True
+        return self._accessor.is_dir(self)
+
+    def is_file(self):
+        self._absolute_path_validation()
+        if not self.exists():
+            return False
+        if not self.bucket or not self.key:
+            return False
+        try:
+            return bool(self.stat())
+        except ClientError:
+            return False
+
+    def iterdir(self):
+        self._absolute_path_validation()
+        yield from super().iterdir()
+
+    @classmethod
+    def cwd(cls):
+        message = cls._NOT_SUPPORTED_MESSAGE.format(method=cls.cwd.__qualname__)
+        raise NotImplementedError(message)
+
+    @classmethod
+    def home(cls):
+        message = cls._NOT_SUPPORTED_MESSAGE.format(method=cls.home.__qualname__)
+        raise NotImplementedError(message)
+
+    def chmod(self, mode):
+        message = self._NOT_SUPPORTED_MESSAGE.format(method=self.chmod.__qualname__)
+        raise NotImplementedError(message)
+
+    def group(self):
+        message = self._NOT_SUPPORTED_MESSAGE.format(method=self.group.__qualname__)
+        raise NotImplementedError(message)
+
+    def is_mount(self):
+        message = self._NOT_SUPPORTED_MESSAGE.format(method=self.is_mount.__qualname__)
+        raise NotImplementedError(message)
+
+    def is_symlink(self):
+        message = self._NOT_SUPPORTED_MESSAGE.format(method=self.is_symlink.__qualname__)
+        raise NotImplementedError(message)
+
+    def is_socket(self):
+        message = self._NOT_SUPPORTED_MESSAGE.format(method=self.is_socket.__qualname__)
+        raise NotImplementedError(message)
+
+    def is_fifo(self):
+        message = self._NOT_SUPPORTED_MESSAGE.format(method=self.is_fifo.__qualname__)
+        raise NotImplementedError(message)
+
+    def is_block_device(self):
+        message = self._NOT_SUPPORTED_MESSAGE.format(method=self.is_block_device.__qualname__)
+        raise NotImplementedError(message)
+
+    def is_char_device(self):
+        message = self._NOT_SUPPORTED_MESSAGE.format(method=self.is_char_device.__qualname__)
+        raise NotImplementedError(message)
+
+
+    def _init(self, template=None):
+        super()._init(template)
+        if template is None:
+            self._accessor = _s3_accessor
+
+    def _absolute_path_validation(self):
+        if not self.is_absolute():
+            raise ValueError('relative path have no bucket, key specification')
---
+++ tests/test_path_operations.py
@@ -0,0 +1,274 @@
+from pathlib import Path
+
+import boto3
+from botocore.exceptions import ClientError
+import pytest
+from moto import mock_s3
+
+from s3path import PureS3Path, S3Path, StatResult, _s3_accessor
+
+
+@pytest.fixture()
+def s3_mock():
+    with mock_s3():
+        _s3_accessor.s3 = boto3.resource('s3')
+        yield
+
+
+def test_path_support():
+    assert PureS3Path in S3Path.mro()
+    assert Path in S3Path.mro()
+
+
+def test_not_supported(monkeypatch):
+    monkeypatch.setattr(S3Path._flavour, 'is_supported', False)
+    error_message = f'cannot instantiate {S3Path.__name__} on your system'
+    with pytest.raises(NotImplementedError, message=error_message):
+        S3Path()
+
+
+def test_cwd():
+    error_message = f'{S3Path.__name__}.cwd() is unsupported on S3 service'
+    with pytest.raises(NotImplementedError, message=error_message):
+        S3Path.cwd()
+
+
+def test_home():
+    error_message = f'{S3Path.__name__}.home() is unsupported on S3 service'
+    with pytest.raises(NotImplementedError, message=error_message):
+        S3Path.home()
+
+
+def test_chmod():
+    error_message = f'{S3Path.__name__}.chmod() is unsupported on S3 service'
+    path = S3Path('/fake-bucket/fake-key')
+    with pytest.raises(NotImplementedError, message=error_message):
+        path.chmod(0o666)
+
+
+def test_group():
+    error_message = f'{S3Path.__name__}.group() is unsupported on S3 service'
+    path = S3Path('/fake-bucket/fake-key')
+    with pytest.raises(NotImplementedError, message=error_message):
+        path.group()
+
+
+def test_is_mount():
+    error_message = f'{S3Path.__name__}.is_mount() is unsupported on S3 service'
+    path = S3Path('/fake-bucket/fake-key')
+    with pytest.raises(NotImplementedError, message=error_message):
+        path.is_mount()
+
+
+def test_is_symlink():
+    error_message = f'{S3Path.__name__}.is_symlink() is unsupported on S3 service'
+    path = S3Path('/fake-bucket/fake-key')
+    with pytest.raises(NotImplementedError, message=error_message):
+        path.is_symlink()
+
+
+def test_is_socket():
+    error_message = f'{S3Path.__name__}.is_socket() is unsupported on S3 service'
+    path = S3Path('/fake-bucket/fake-key')
+    with pytest.raises(NotImplementedError, message=error_message):
+        path.is_socket()
+
+
+def test_is_fifo():
+    error_message = f'{S3Path.__name__}.is_fifo() is unsupported on S3 service'
+    path = S3Path('/fake-bucket/fake-key')
+    with pytest.raises(NotImplementedError, message=error_message):
+        path.is_fifo()
+
+
+def test_is_block_device():
+    error_message = f'{S3Path.__name__}.is_block_device() is unsupported on S3 service'
+    path = S3Path('/fake-bucket/fake-key')
+    with pytest.raises(NotImplementedError, message=error_message):
+        path.is_block_device()
+
+
+def test_is_char_device():
+    error_message = f'{S3Path.__name__}.is_char_device() is unsupported on S3 service'
+    path = S3Path('/fake-bucket/fake-key')
+    with pytest.raises(NotImplementedError, message=error_message):
+        path.is_char_device()
+
+
+def test_stat(s3_mock):
+    path = S3Path('fake-bucket/fake-key')
+    with pytest.raises(ValueError):
+        path.stat()
+
+    path = S3Path('/fake-bucket/fake-key')
+    with pytest.raises(ClientError):
+        path.stat()
+
+    s3 = boto3.resource('s3')
+    s3.create_bucket(Bucket='test-bucket')
+    simple_object = s3.ObjectSummary('test-bucket', 'Test.test')
+    simple_object.put(Body=b'test data')
+
+    path = S3Path('/test-bucket/Test.test')
+    stat = path.stat()
+    assert isinstance(stat, StatResult)
+    assert stat == StatResult(
+        size=simple_object.size,
+        last_modified=simple_object.last_modified,
+    )
+
+    path = S3Path('/test-bucket')
+    assert path.stat() is None
+
+
+def test_exists(s3_mock):
+    path = S3Path('./fake-key')
+    with pytest.raises(ValueError):
+        path.exists()
+
+    path = S3Path('/fake-bucket/fake-key')
+    with pytest.raises(ClientError):
+        path.exists()
+
+    s3 = boto3.resource('s3')
+    s3.create_bucket(Bucket='test-bucket')
+    simple_object = s3.ObjectSummary('test-bucket', 'directory/Test.test')
+    simple_object.put(Body=b'test data')
+
+    assert not S3Path('/test-bucket/Test.test').exists()
+    path = S3Path('/test-bucket/directory/Test.test')
+    assert path.exists()
+    for parent in path.parents:
+        assert parent.exists()
+
+
+def test_glob(s3_mock):
+    s3 = boto3.resource('s3')
+    s3.create_bucket(Bucket='test-bucket')
+    simple_object = s3.ObjectSummary('test-bucket', 'directory/Test.test')
+    simple_object.put(Body=b'test data')
+
+    assert list(S3Path('/test-bucket/').glob('*.test')) == []
+    assert list(S3Path('/test-bucket/directory/').glob('*.test')) == [S3Path('/test-bucket/directory/Test.test')]
+    assert list(S3Path('/test-bucket/').glob('**/*.test')) == [S3Path('/test-bucket/directory/Test.test')]
+    assert list(S3Path('/test-bucket/').rglob('*.test')) == [S3Path('/test-bucket/directory/Test.test')]
+    assert list(S3Path('/test-bucket/').rglob('**/*.test')) == [S3Path('/test-bucket/directory/Test.test')]
+
+    simple_object = s3.ObjectSummary('test-bucket', 'pathlib.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'setup.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'test_pathlib.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'docs/conf.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'build/lib/pathlib.py')
+    simple_object.put(Body=b'test data')
+
+    assert sorted(S3Path.from_uri('s3://test-bucket/').glob('*.py')) == [
+        S3Path('/test-bucket/pathlib.py'),
+        S3Path('/test-bucket/setup.py'),
+        S3Path('/test-bucket/test_pathlib.py')]
+    assert sorted(S3Path.from_uri('s3://test-bucket/').glob('*/*.py')) == [S3Path('/test-bucket/docs/conf.py')]
+    assert sorted(S3Path.from_uri('s3://test-bucket/').glob('**/*.py')) == [
+        S3Path('/test-bucket/build/lib/pathlib.py'),
+        S3Path('/test-bucket/docs/conf.py'),
+        S3Path('/test-bucket/pathlib.py'),
+        S3Path('/test-bucket/setup.py'),
+        S3Path('/test-bucket/test_pathlib.py')]
+
+
+def test_is_dir(s3_mock):
+    s3 = boto3.resource('s3')
+    s3.create_bucket(Bucket='test-bucket')
+    simple_object = s3.ObjectSummary('test-bucket', 'directory/Test.test')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'pathlib.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'setup.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'test_pathlib.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'docs/conf.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'build/lib/pathlib.py')
+    simple_object.put(Body=b'test data')
+
+    assert not S3Path('/test-bucket/fake.test').is_dir()
+    assert not S3Path('/test-bucket/fake/').is_dir()
+    assert S3Path('/test-bucket/directory').is_dir()
+    assert not S3Path('/test-bucket/directory/Test.test').is_dir()
+    assert not S3Path('/test-bucket/pathlib.py').is_dir()
+    assert not S3Path('/test-bucket/docs/conf.py').is_dir()
+    assert S3Path('/test-bucket/docs/').is_dir()
+    assert S3Path('/test-bucket/build/').is_dir()
+    assert S3Path('/test-bucket/build/lib').is_dir()
+    assert not S3Path('/test-bucket/build/lib/pathlib.py').is_dir()
+
+
+def test_is_file(s3_mock):
+    s3 = boto3.resource('s3')
+    s3.create_bucket(Bucket='test-bucket')
+    simple_object = s3.ObjectSummary('test-bucket', 'directory/Test.test')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'pathlib.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'setup.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'test_pathlib.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'docs/conf.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'build/lib/pathlib.py')
+    simple_object.put(Body=b'test data')
+
+    assert not S3Path('/test-bucket/fake.test').is_file()
+    assert not S3Path('/test-bucket/fake/').is_file()
+    assert not S3Path('/test-bucket/directory').is_file()
+    assert S3Path('/test-bucket/directory/Test.test').is_file()
+    assert S3Path('/test-bucket/pathlib.py').is_file()
+    assert S3Path('/test-bucket/docs/conf.py').is_file()
+    assert not S3Path('/test-bucket/docs/').is_file()
+    assert not S3Path('/test-bucket/build/').is_file()
+    assert not S3Path('/test-bucket/build/lib').is_file()
+    assert S3Path('/test-bucket/build/lib/pathlib.py').is_file()
+
+
+def test_iterdir(s3_mock):
+    s3 = boto3.resource('s3')
+    s3.create_bucket(Bucket='test-bucket')
+    simple_object = s3.ObjectSummary('test-bucket', 'directory/Test.test')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'pathlib.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'setup.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'test_pathlib.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'build/lib/pathlib.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'docs/conf.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'docs/make.bat')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'docs/index.rst')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'docs/Makefile')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'docs/_templates/11conf.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'docs/_build/22conf.py')
+    simple_object.put(Body=b'test data')
+    simple_object = s3.ObjectSummary('test-bucket', 'docs/_static/conf.py')
+    simple_object.put(Body=b'test data')
+
+    s3_path = S3Path('/test-bucket/docs')
+    assert sorted(s3_path.iterdir()) == [
+        S3Path('/test-bucket/docs/Makefile'),
+        S3Path('/test-bucket/docs/_build'),
+        S3Path('/test-bucket/docs/_static'),
+        S3Path('/test-bucket/docs/_templates'),
+        S3Path('/test-bucket/docs/conf.py'),
+        S3Path('/test-bucket/docs/index.rst'),
+        S3Path('/test-bucket/docs/make.bat'),
+    ]
