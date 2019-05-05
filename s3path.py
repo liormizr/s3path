cc630deca590b9f7ea7fe1e86fc5cacaebec5d6a
lior <li.mizr@gmail.com>
2019-05-05 13:08:11 +0300

    new version with owner method fix

--- s3path.py
+++ s3path.py
@@ -19,7 +19,7 @@ except ImportError:
     StreamingBody = object
     LazyLoadedDocstring = type(None)
 
-__version__ = '0.1.05'
+__version__ = '0.1.06'
 __all__ = (
     'register_configuration_parameter',
     'S3Path',
