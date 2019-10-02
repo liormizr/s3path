cf1b7fc374cdd8570b0b0dc3363b15c29b51aa02
liormizr <li.mizr@gmail.com>
2019-10-02 21:45:27 +0300

    version 0.1.07 with adding pagination to scandir

--- s3path.py
+++ s3path.py
@@ -19,7 +19,7 @@ except ImportError:
     StreamingBody = object
     LazyLoadedDocstring = type(None)
 
-__version__ = '0.1.06'
+__version__ = '0.1.07'
 __all__ = (
     'register_configuration_parameter',
     'S3Path',
