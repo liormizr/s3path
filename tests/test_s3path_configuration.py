c91f0929dc71cabd22ee81417d4049368090e62c
liormizr <li.mizr@gmail.com>
2020-09-08 14:15:14 +0300

    fix hierarchical_configuration test

--- tests/test_s3path_configuration.py
+++ tests/test_s3path_configuration.py
@@ -4,4 +4,5 @@ from s3path import S3Path, register_configuration_parameter, _s3_accessor
 def test_hierarchical_configuration():
     path = S3Path('/foo/')
     register_configuration_parameter(path, parameters={'ContentType': 'text/html'})
-    assert _s3_accessor.configuration_map == {path: {'ContentType': 'text/html'}}
+    assert path in _s3_accessor.configuration_map
+    assert _s3_accessor.configuration_map[path] == {'ContentType': 'text/html'}
