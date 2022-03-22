c85c02555c7e839c443c1960ed034724bb74aba1
liormizr <li.mizr@gmail.com>
2022-03-22 19:04:35 +0200

    New Version 0.3.4

--- s3path.py
+++ s3path.py
@@ -26,7 +26,7 @@ except ImportError:
     smart_open = None
     ALLOWED_COPY_ARGS = []
 
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 __all__ = (
     'register_configuration_parameter',
     'S3Path',
