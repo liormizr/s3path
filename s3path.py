1fcba773a1bdf5bb2bd1c2ad96a5e0e0d4282ae1
Nick Langellier <nick@videa.ai>
2022-11-02 22:35:58 -0400

    remove deprecated distutils

--- s3path.py
+++ s3path.py
@@ -7,7 +7,6 @@ from functools import lru_cache
 from contextlib import suppress
 from collections import namedtuple
 from platform import python_version
-from distutils.version import StrictVersion
 from io import DEFAULT_BUFFER_SIZE, UnsupportedOperation
 from pathlib import _PosixFlavour, _Accessor, PurePath, Path
 from threading import Lock
@@ -16,6 +15,7 @@ try:
     import boto3
     from botocore.exceptions import ClientError
     from botocore.docs.docstring import LazyLoadedDocstring
+    from packaging.version import Version
     import smart_open
     from s3transfer.manager import TransferManager
     ALLOWED_COPY_ARGS = TransferManager.ALLOWED_COPY_ARGS
@@ -24,6 +24,7 @@ except ImportError:
     ClientError = Exception
     LazyLoadedDocstring = None
     smart_open = None
+    Version = None
     ALLOWED_COPY_ARGS = []
 
 __version__ = '0.3.4'
@@ -784,7 +785,7 @@ class S3Path(_PathNotSupportedMixin, Path, PureS3Path):
         # to raise a `FileNotFoundError`, we need to manually check if the file exists
         # before we make the API call -- since we want to delete the file anyway,
         # we can just ignore this for now and be satisfied that the file will be removed
-        if StrictVersion(python_version()) < StrictVersion('3.8'):
+        if Version(python_version()) < Version('3.8'):
             return super().unlink()
         super().unlink(missing_ok=missing_ok)
 
